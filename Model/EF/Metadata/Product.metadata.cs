f2d76ace488adfb9f3ec4f6cde146278cb7b99e9
linhdn <linhdn@dkt.com.vn>
2017-11-11 19:47:44 +0700

    first metadata for Product

--- Model/EF/Metadata/Product.metadata.cs
+++ Model/EF/Metadata/Product.metadata.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//using 2 thư viện thiết kế class metadata
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Model.EF
@@ -18,78 +12,49 @@ namespace Model.EF
         //sealed khong cho phep ke thua
         internal sealed class ProductMetadata
         {
-            [Display(Name = "")]
+            [Display(Name = "Mã")]
             public int idProduct { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Tên")]
+            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
             public string name { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Mã hàng")]
+            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
             public string code { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Mô tả")]
+            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
             public string description { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Ảnh")]
+            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
             public string image { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Ảnh dải")]
             public string moreImage { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Giá")]
+            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
             public Nullable<decimal> price { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Giá khuyến mãi")]
             public Nullable<decimal> promotionPrice { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Chi tiết")]
             public string detail { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Loại")]
+            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
             public Nullable<int> idCategory { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Bảo hành")]
             public Nullable<int> wanranty { get; set; }
 
-            [Display(Name = "")]
+            [Display(Name = "Nhà cung cấp")]
             public Nullable<int> idSupplier { get; set; }
 
-
-            [Display(Name = "Ảnh bìa")]
-            public string Image { get; set; }
-
-            [Display(Name = "Mã")]
-            public int IDBaiDang { get; set; }
-
-            [Display(Name = "Chủ đề lớn")]
-            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
-            public Nullable<int> IDLoaiBaiDang { get; set; }
-
-            [Display(Name = "Chủ đề nhỏ")]
-            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
-            public Nullable<int> IDLoaiCon { get; set; }
-
-            [Display(Name = "Tiêu đề")]
-            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
-            public string TieuDe { get; set; }
-
-            [Display(Name = "Nội dung")]
-            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
-            public string NoiDung { get; set; }
-
-            [Display(Name = "Miêu tả")]
-            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
-            public string MieuTa { get; set; }
-
-            [Display(Name = "Trạng thái")]
-            [Required(ErrorMessage = "Vui lòng nhập dữ liệu cho trường này.")]
-            public string TrangThai { get; set; }
-
-            [Display(Name = "Ngày viết")]
-            [DataType(DataType.Date)]
-            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-            public Nullable<System.DateTime> NgayViet { get; set; }
-
+            
         }
     }
 }
